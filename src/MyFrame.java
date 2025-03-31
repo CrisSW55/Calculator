6763792c0e8d6ad6f102688bf721914574feadd1
Crystian Guzman <crystianguzman55@gmail.com>
2025-03-30 23:29:20 -0700

    adjusted the layout so the componentes suchas the JButtons and Textfield are placed correclty on the JFrame. This involved refactoring the code and removing the MainPanel class. Lastly added the ActionListener interface onto the MyFrame class, so the JButtons take events that are  register to the listener. Also, passed the MyFrame's ActionListener using the keyword 'this' place into the NumPadPanel constructor's argument.

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,16 +1,55 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFrame extends JFrame {
-    MainPanel mainPanel;
+public class MyFrame extends JFrame implements ActionListener {
+    TextField textField;
+    NumPadPanel numPadPanel;
+    JButton bNeg;
+    JButton bDel;
+    JButton bClear;
+
     public MyFrame() {
-        mainPanel = new MainPanel();
         setTitle("Calculator");
-        setSize(new Dimension(500,700));
+        setSize(new Dimension(500,600));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        textField = new TextField();
+        textField.setFont(new Font("Ink Free",Font.BOLD,30));
+        textField.setFocusable(false);
+        textField.setEditable(false);
+        textField.setBounds(100,50,300,50);
+
+        numPadPanel = new NumPadPanel(this);
+
+        bNeg = new JButton("(-)");
+        bNeg.setFocusable(false);
+        bNeg.addActionListener(this);
+        bDel = new JButton("Delete");
+        bDel.setFocusable(false);
+        bDel.addActionListener(this);
+        bClear = new JButton("Clear");
+        bClear.setFocusable(false);
+        bClear.addActionListener(this);
+
+        bNeg.setBounds(100,500,100,50);
+        bDel.setBounds(200,500,100,50);
+        bClear.setBounds(300,500,100,50);
+
+        add(textField);
+        add(numPadPanel);
+        add(bNeg);
+        add(bDel);
+        add(bClear);
+
         setLocationRelativeTo(null);
         setVisible(true);
-        mainPanel.setLayout(new GridLayout(3,1));
-        add(mainPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
--- src/NumPadPanel.java
+++ src/NumPadPanel.java
@@ -1,32 +1,52 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class NumPadPanel extends JPanel {
     JButton b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,
             bMinus,bPlus,bMult,bDiv,bEqual,bDec;
-    public NumPadPanel() {
+    private ActionListener actionListener;
+    public NumPadPanel(ActionListener actionListener) {
+        this.actionListener = actionListener;
+        setBounds(100,150,300,300);
+        setLayout(new GridLayout(4,4,10,10));
+
         b1 = new JButton("1");
+        b1.setFocusable(false);
         b2 = new JButton("2");
+        b2.setFocusable(false);
         b3 = new JButton("3");
+        b3.setFocusable(false);
         bPlus = new JButton("+");
+        bPlus.setFocusable(false);
 
         b4 = new JButton("4");
+        b4.setFocusable(false);
         b5 = new JButton("5");
+        b5.setFocusable(false);
         b6 = new JButton("6");
+        b6.setFocusable(false);
         bMinus = new JButton("-");
+        bMinus.setFocusable(false);
 
         b7 = new JButton("7");
+        b7.setFocusable(false);
         b8 = new JButton("8");
+        b8.setFocusable(false);
         b9 = new JButton("9");
+        b9.setFocusable(false);
         bMult = new JButton("*");
+        bMult.setFocusable(false);
 
         bDec = new JButton(".");
+        bDec.setFocusable(false);
         b0 = new JButton("0");
+        b0.setFocusable(false);
         bEqual = new JButton("=");
+        bEqual.setFocusable(false);
         bDiv = new JButton("/");
-        setBackground(Color.LIGHT_GRAY);
-        setPreferredSize(new Dimension(300,300));
-        setLayout(new GridLayout(4,4,5,5));
+        bDiv.setFocusable(false);
+
         add(b1);add(b2);add(b3);add(bPlus);
         add(b4);add(b5);add(b6);add(bMinus);
         add(b7);add(b8);add(b9);add(bMult);
